705972bbb5937f41a9dccca9fcc2f43f19ac048d
fernandoMaia0 <fernandomaia.dev@gmail.com>
2024-12-03 15:35:46 -0300

    criacao de rota para recomendar pc

--- tp_ia/app_ia/urls.py
+++ tp_ia/app_ia/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'app_ia'
+
 urlpatterns = [
     path('',views.index),
+    path('recommend/',views.recommend, name='recommend'),
 ]
+
